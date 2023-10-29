6ee5841ccd2c9852884422e36536d8b190077bf9
Paritosh Mathur <117584083+paritxshh@users.noreply.github.com>
2023-10-29 18:52:45 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (28.18%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int pigs = 0;
+        int tests = minutesToTest / minutesToDie + 1;
+        
+        while (Math.pow(tests, pigs) < buckets) {
+            pigs++;
+        }
+        
+        return pigs;
+    }
+}
