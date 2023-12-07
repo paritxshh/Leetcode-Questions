11e5c8f07186208770bfc91e4ba7fde23203dd55
Paritosh Mathur <117584083+paritxshh@users.noreply.github.com>
2023-12-07 09:55:33 +0530

    Time: 101 ms (67.62%), Space: 79.3 MB (45.63%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -1,46 +1,28 @@
-class T {
-  public int procTime;
-  public int index;
-
-  public T(int procTime, int index) {
-    this.procTime = procTime;
-    this.index = index;
-  }
-}
-
 class Solution {
-  public int[] getOrder(int[][] tasks) {
-    final int n = tasks.length;
-    int[][] A = new int[n][3];
-
-    for (int i = 0; i < n; ++i) {
-      A[i][0] = tasks[i][0];
-      A[i][1] = tasks[i][1];
-      A[i][2] = i;
+    public int[] getOrder(int[][] tasks) {
+        int n = tasks.length;
+        int[][] ts = new int[n][3];
+        for (int i = 0; i < n; ++i) {
+            ts[i] = new int[] {tasks[i][0], tasks[i][1], i};
+        }
+        
+        Arrays.sort(ts, (a,b) -> a[0] - b[0]);
+        int[] ans = new int[n];
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        int i = 0, t = 0, k = 0;
+        while (!q.isEmpty() || i < n) {
+            if (q.isEmpty()) {
+                t = Math.max(t, ts[i][0]);
+            }
+            while (i < n && ts[i][0] <= t) {
+                q.offer(new int[] {ts[i][1], ts[i][2]});
+                ++i;
+            }
+            var p = q.poll();
+            ans[k++] = p[1];
+            t += p[0];
+        }
+        
+        return ans;
     }
-
-    int[] ans = new int[n];
-    int ansIndex = 0;
-    Queue<T> minHeap = new PriorityQueue<>(
-        (a, b) -> a.procTime == b.procTime ? a.index - b.index : a.procTime - b.procTime);
-    int i = 0;     // tasks' pointer
-    long time = 0; // current time
-
-    Arrays.sort(A, Comparator.comparing(a -> a[0]));
-
-    while (i < n || !minHeap.isEmpty()) {
-      if (minHeap.isEmpty())
-        time = Math.max(time, (long) A[i][0]);
-      while (i < n && time >= (long) A[i][0]) {
-        minHeap.offer(new T(A[i][1], A[i][2]));
-        ++i;
-      }
-      final int procTime = minHeap.peek().procTime;
-      final int index = minHeap.poll().index;
-      time += procTime;
-      ans[ansIndex++] = index;
-    }
-
-    return ans;
-  }
 }
